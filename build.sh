e23f5012763d771ff8e61c4838c670817e24e7b3
Patrik Beyls <patrik.beyls@worldline.com>
2020-11-21 22:00:15 +0100

    guestfish: upload ssh public key

--- build.sh
+++ build.sh
@@ -17,9 +17,10 @@ sudo guestfish -a out/$LAVA.img --rw << __EOF__
 # start the guestfs VM
 run
 
-# upload cloud-init config
+# upload cloud-init config and ssh public key
 mount /dev/sda1 /
 upload assets/user-data /user-data
+upload assets/.ssh/id_rsa.pub /id_rsa.pub
 umount /dev/sda1
 
 __EOF__
